c9150bff47df290352b068dcb14dfbb4831d5be0
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2021-11-09 18:00:07 -0800

    deployment setup

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("4KCfsDf6inyHaGCuvdNEYYCPGrh7cxqMwqTRivAiShX6");
 
 #[program]
 pub mod myepicproject {
